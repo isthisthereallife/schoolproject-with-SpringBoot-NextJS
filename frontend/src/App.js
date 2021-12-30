7c06295a36dd70c3a1b9296dfea8ab6a43e37c74
Astrid Stenvall <astrid.ingeborg@gmail.com>
2021-12-30 12:26:34 +0100

    deleted unwanted classes etc

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h2>Städa fint AB</h2>
+        <p>Har vunnit förstapris i bäst städning 150 år i rad!</p>
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
