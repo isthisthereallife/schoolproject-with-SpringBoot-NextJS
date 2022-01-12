97a852ef5426abb69eb6b8eac01c64469410c4d5
Magnus <isthisthereallife@riseup.net>
2022-01-12 15:21:50 +0100

    removing redundant files

--- frontend/services/BookingService.jsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-const BOOKING_REST_API_URL = 'http://localhost:8080/booking'
-class BookService {
-    getBookings() {
-        return this.axios.get(BOOKING_REST_API_URL);
-    }
-}
-export default new BookService();
